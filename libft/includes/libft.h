9adae5af8aa7216d6a37342ee4dd857fa81202c9
Heidy BEN YAHIA <hben-yah@student.42.fr>
2019-02-02 12:40:11 +0100

    libft header

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: hben-yah <hben-yah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/08 18:00:17 by hben-yah          #+#    #+#             */
-/*   Updated: 2018/12/23 16:58:45 by hben-yah         ###   ########.fr       */
+/*   Updated: 2019/02/02 12:39:56 by hben-yah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,5 @@
 
 # include <stdlib.h>
 
-# include "ft_boolean.h"
-# include "ft_char.h"
-# include "ft_list.h"
-# include "ft_collection.h"
-# include "ft_convert.h"
-# include "ft_input.h"
-# include "ft_integer.h"
-# include "ft_math.h"
-# include "ft_memory.h"
-# include "ft_misc.h"
-# include "ft_string.h"
-# include "ft_output.h"
-# include "ft_sort.h"
-# include "ft_table.h"
 
 #endif
